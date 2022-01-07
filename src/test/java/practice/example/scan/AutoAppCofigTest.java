2cefdb23cdcecf303b991f2e813384c79eaac1f6
Jisu Han <weblite@naver.com>
2022-01-07 14:21:39 +0900

    @Qualifier 테스트

--- src/test/java/practice/example/scan/AutoAppCofigTest.java
+++ src/test/java/practice/example/scan/AutoAppCofigTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import practice.example.AutoAppConfig;
+import practice.example.discount.DiscountPolicy;
+import practice.example.discount.RateDiscountPolicy;
 import practice.example.member.MemberService;
 import practice.example.order.OrderService;
 import practice.example.order.OrderServiceImpl;
@@ -17,6 +19,10 @@ public class AutoAppCofigTest {
     void basicScan(){
         MemberService memberService=ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        DiscountPolicy discountPolicy=ac.getBean(DiscountPolicy.class);
+        Assertions.assertThat(discountPolicy).isInstanceOf(RateDiscountPolicy.class);
+
 
         OrderService orderService=ac.getBean(OrderServiceImpl.class);
         Assertions.assertThat(orderService).isInstanceOf(OrderServiceImpl.class);
