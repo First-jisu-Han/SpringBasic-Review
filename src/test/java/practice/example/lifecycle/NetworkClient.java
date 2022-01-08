911d14373a67a2afaa1fbec69f94d219fe9f64dc
Jisu Han <weblite@naver.com>
2022-01-08 12:31:44 +0900

    빈 생명주기 콜백 지원 3.@PreDestroy , @PostConstruct 빈 생명주기에는 이것을 사용하도록함

--- src/test/java/practice/example/lifecycle/NetworkClient.java
+++ src/test/java/practice/example/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package practice.example.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url;
@@ -25,11 +28,15 @@ public class NetworkClient  {
     public void disconnect() {
         System.out.println("close = " + url);
     }
+
+
+    @PostConstruct
     public void init() {
-        System.out.println("NetworkClient.init " );
+        System.out.println("NetworkClient.init" );
         connect();
         call("초기화메세지: ");
     }
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
